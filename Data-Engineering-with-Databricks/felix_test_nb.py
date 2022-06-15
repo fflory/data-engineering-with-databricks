62097cea6ebf3af9455d585fde9fc70731084c3a
fflory <fflory>
2022-06-15 15:42:48 +0000

    pip test added

---
+++ Data-Engineering-with-Databricks/felix_test_nb.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %pip install \
+# MAGIC git+https://github.com/databricks-academy/dbacademy-gems \
+# MAGIC git+https://github.com/databricks-academy/dbacademy-rest \
+# MAGIC --quiet --disable-pip-version-check
+
+# COMMAND ----------
+
+
